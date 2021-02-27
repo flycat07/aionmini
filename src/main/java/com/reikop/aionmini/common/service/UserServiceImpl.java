aa6c98840b44bc5a5df1ca01ec540c4f7610b783
박성현 <reikop.com@gmail.com>
2021-02-27 12:10:41 +0900

    유저 저장 안함

--- src/main/java/com/reikop/aionmini/common/service/UserServiceImpl.java
+++ src/main/java/com/reikop/aionmini/common/service/UserServiceImpl.java
@@ -3,24 +3,38 @@ package com.reikop.aionmini.common.service;
 import com.reikop.aionmini.common.repo.UserRepo;
 import com.reikop.aionmini.work.model.User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Service
+@Transactional
 public class UserServiceImpl implements UserService {
 
     @Resource
     private UserRepo repo;
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @Override
     public User putUser(User user) {
-        User byServerAndUserid = repo.findByServerAndUserid(user.getServer(), user.getUserid());
-        if(byServerAndUserid != null){
-            byServerAndUserid.setCharname(user.getCharname());
-            return repo.save(byServerAndUserid);
-        }else{
-            return repo.save(user);
-        }
+//        User byServerAndUserid = repo.findByServerAndUserid(user.getServer(), user.getUserid());
+//        if(byServerAndUserid != null){
+//            byServerAndUserid.setCharname(user.getCharname());
+//        }
+//        return entityManager.merge(user);
+    return user;
+//        if(byServerAndUserid != null){
+//            byServerAndUserid.setCharname(user.getCharname());
+//            return repo.save(byServerAndUserid);
+//        }else{
+//            return repo.save(user);
+//        }
+//        entityManager.merge(user);
+
     }
 //
 //    @Override
